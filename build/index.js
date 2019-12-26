48320344ada0a85f0dff7fa78239359d671029af
“John <john@reactnative.guru>
2019-12-26 19:37:17 +0000

    base express server

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const express = require('express');
+const app = express();
+const port = 9000;
+app.get('/', (req, res) => res.send('hello world'));
+app.listen(port);
+console.log(`[app] : http://localhost:${port}`);
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express' 
+import bodyParser from "body-parser";
+
+import { listings } from "./listings";
+
+const app = express();
+const port = 9000;
+app.use(bodyParser.json());
+
+
+app.get("/listings", (_req, res) => {
+    res.send(listings);
+  });
+
+app.get('/', (_req, res) => res.send('hello world'));
+
+app.post("/delete-listing", (req, res) => {
+    const id: string = req.body.id;
+  
+    for (let i = 0; i < listings.length; i++) {
+      if (listings[i].id === id) {
+        return res.send(listings.splice(i, 1)[0]);
+      }
+    }
+  
+    return res.send("failed to deleted listing");
+  });
+
+app.listen(port);
+
+console.log(`[app] : http://localhost:${port}`);
